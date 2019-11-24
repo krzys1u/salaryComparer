68daddbb782f596b07807d66ed8d2c66132c7e15
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2019-11-24 17:29:01 +0100

    Updates

---
+++ precache-manifest.6e5a89d49209ffb660d409a505564fbe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eca7ec3e81cfff8e4d66fcfbb89dcb5b",
+    "url": "/salary-comparer/index.html"
+  },
+  {
+    "revision": "a7e8f80281bd077e996c",
+    "url": "/salary-comparer/static/css/main.31ce0702.chunk.css"
+  },
+  {
+    "revision": "6780e4cabdea56d75985",
+    "url": "/salary-comparer/static/js/2.abf8953f.chunk.js"
+  },
+  {
+    "revision": "a7e8f80281bd077e996c",
+    "url": "/salary-comparer/static/js/main.098261ad.chunk.js"
+  },
+  {
+    "revision": "43737804f3367e4f6dcf",
+    "url": "/salary-comparer/static/js/runtime-main.50ed7340.js"
+  }
+]);
