17e64e8ba0787fc49ce1c42a346488161e32a92d
Krzysztof Stozek <stozekrzysztof@gmail.com>
2019-11-28 15:45:16 +0000

    Updates

---
+++ precache-manifest.6c2f7f22efa912574bcf5d07d2eae9fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c015bf23cae87b5e4ce1393b996254fc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ae1e0b3684e408b76c6",
+    "url": "/static/css/main.31ce0702.chunk.css"
+  },
+  {
+    "revision": "6780e4cabdea56d75985",
+    "url": "/static/js/2.abf8953f.chunk.js"
+  },
+  {
+    "revision": "7ae1e0b3684e408b76c6",
+    "url": "/static/js/main.cad99a06.chunk.js"
+  },
+  {
+    "revision": "351ad17936292c84e311",
+    "url": "/static/js/runtime-main.beab58bd.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/salary-comparer/precache-manifest.6e5a89d49209ffb660d409a505564fbe.js"
+  "/precache-manifest.6c2f7f22efa912574bcf5d07d2eae9fe.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/salary-comparer/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
