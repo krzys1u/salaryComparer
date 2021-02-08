881e8eb8a5d50bd312ec793b4d44a1d55859b732
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2021-02-08 14:40:03 +0100

    next version of firestore

--- api/metadata.js
+++ api/metadata.js
@@ -1,14 +1,28 @@
 const admin = require('firebase-admin')
+const { FIREBASE_URL } = require('../src/config')
 
-module.exports = async (req, res) => {
+const initializeFirestore = () => {
   admin.initializeApp({
-    // apiKey: process.env.GCLOUD_CREDENTIALS
-    // credential: admin.credential.applicationDefault()
+    credential: admin.credential.cert({
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    }),
+    databaseURL: process.env.FIREBASE_URL,
   })
 
-  const versionDocument = admin.firestore().db.collection('meta').doc('version')
+  return admin.firestore().db
+}
 
-  const version = (await versionDocument.get()).data()
+module.exports = async (req, res) => {
+  const firestore = initializeFirestore()
+
+  const versionDocument = await firestore
+    .collection('meta')
+    .doc('version')
+    .get()
+
+  const version = versionDocument.data()
 
   res.json({
     ...version,
