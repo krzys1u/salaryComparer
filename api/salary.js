bf340546b291952057d160c72ed26b5636f54fb1
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2021-02-09 15:02:06 +0100

    use standard api path

--- api/salary.js
+++ api/salary.js
@@ -1,12 +1,1 @@
-const { firebaseService } = require('../services/FirebaseService')
-
-const collection = 'salary-data-1'
-const document = '1000-uop-0'
-
-module.exports = async (req, res) => {
-  const version = await firebaseService.get({ collection, document })
-
-  res.json({
-    ...version,
-  })
-}
+module.exports = require('../src/api/salary')
