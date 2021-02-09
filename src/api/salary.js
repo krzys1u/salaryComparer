4c1aab9d988fa411a1bce4a595ab3a526881d608
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2021-02-09 14:56:38 +0100

    change cache settings

--- src/api/salary.js
+++ src/api/salary.js
@@ -1,12 +1,15 @@
 const { firebaseService } = require('../services/FirebaseService')
 
-const collection = 'salary-data-1'
 const document = '1000-uop-0'
 
 module.exports = async (req, res) => {
-  const version = await firebaseService.get({ collection, document })
+  const { version } = await firebaseService.get({collection: 'meta', document: 'version'});
+
+  const collection = `salary-data-${version}`;
+
+  const data = await firebaseService.get({ collection, document })
 
   res.json({
-    ...version,
+    ...data,
   })
 }
