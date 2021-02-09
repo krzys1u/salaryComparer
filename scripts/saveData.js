7086ac24f07d1225303362db980a7d23dba463fb
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2021-02-09 14:47:43 +0100

    add firebase service and use it

--- scripts/saveData.js
+++ scripts/saveData.js
@@ -1,71 +1,72 @@
-const admin = require('firebase-admin');
+const admin = require('firebase-admin')
 
-const fetchData = require('./fetchData');
-const { FIREBASE_URL } = require('../src/config');
+const fetchData = require('./fetchData')
+const { createFirebaseService } = require('../src/services/FirebaseService')
+const { FIREBASE_URL } = require('../src/config')
 
 admin.initializeApp({
   credential: admin.credential.cert(require('../authKey.json')),
   databaseURL: FIREBASE_URL,
-});
+})
 
-const db = admin.firestore();
+const db = admin.firestore()
+
+const firebase = createFirebaseService(db)
 
 const createVersionData = (version, created) => ({
   version,
   created,
-});
+})
 
-const getCollectionName = (version) => `salary-data-${version}`;
+const getCollectionName = (version) => `salary-data-${version}`
 
-const getRecordId = (record) => `${record.brutto}-${record.type}`;
+const getRecordId = (record) => `${record.brutto}-${record.type}`
 
 const saveRecord = async (collection, key, record) => {
-  const document = db.collection(collection).doc(key);
-
-  await document.set(record);
-};
-
-const getRecord = async (collection, key) => {
-  const document = db.collection(collection).doc(key);
-  const data = await document.get();
-
-  return data.data();
-};
+  await firebase.set({ collection, key, record })
+}
 
 const getCurrentVersion = async () => {
-  const versionData = await getRecord('meta', 'version');
+  const versionData = await firebase.get({
+    collection: 'meta',
+    document: 'version',
+  })
 
-  return versionData.version;
-};
+  return versionData.version
+}
 
 const saveData = async (collectionName, data) => {
-  return Promise.all(data.map((record) => saveRecord(collectionName, getRecordId(record), record)));
-};
+  return Promise.all(
+    data.map((record) =>
+      saveRecord(collectionName, getRecordId(record), record),
+    ),
+  )
+}
 
 const updateVersion = async (version) => {
-  const versionData = createVersionData(version, new Date().getTime());
+  const versionData = createVersionData(version, new Date().getTime())
 
-  await saveRecord('meta', 'version', versionData);
-};
+  await saveRecord('meta', 'version', versionData)
+}
 
 const createNewVersion = async (data) => {
-  const version = await getCurrentVersion();
-  const nextVersion = version + 1;
+  const version = await getCurrentVersion()
+  const nextVersion = version + 1
 
-  const collectionName = getCollectionName(nextVersion);
+  const collectionName = getCollectionName(nextVersion)
 
   try {
-    await saveData(collectionName, data);
-    await updateVersion(nextVersion);
+    await saveData(collectionName, data)
+    await updateVersion(nextVersion)
 
-    console.info('New data has been saved');
+    console.info('New data has been saved')
   } catch (e) {
-    console.error('Error during saving data', e);
+    console.error('Error during saving data', e)
   }
-};
+}
 
-(async () => {
-  const data = await fetchData();
+;(async () => {
+  const data = await fetchData()
 
-  await createNewVersion(data);
-})();
+  await createNewVersion(data)
+})()
