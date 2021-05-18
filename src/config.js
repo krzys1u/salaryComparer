184c7baf1265ff3ee946531398a479b52eb86ace
Krzysztof Stozek <krzysztof.stozek@grandparade.co.uk>
2021-05-18 23:34:21 +0200

    change max salaries to 50k

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   FIREBASE_URL: 'https://salario-a6a4b.firebaseio.com',
   API_URL: 'https://salario.vercel.app',
   SALARY_MIN: 2000,
-  SALARY_MAX: 30000,
+  SALARY_MAX: 50000,
   SALARY_STEP: 250,
   SALARY_SLIDER_STEP: 500,
   CREATIVE_RIGHTS_STEPS: [0, 50, 60, 70, 80],
